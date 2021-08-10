3371e9932428ad48d0cc220aa66ca54c5df46986
Zouhair Jabiri <zouhairjab0@gmail.com>
2021-08-10 22:04:31 +0100

    database

--- sqlHelpers/uninstall.php
+++ sqlHelpers/uninstall.php
@@ -2,7 +2,6 @@
 
 $sql = array();
 
-// Je ne joue pas la requête de suppression de la colonne pour éviter les erreurs. Il faudra la désinstaller manuellement
 $sql[] = "DROP TABLE IF EXISTS `" . _DB_PREFIX_ . "client_review`;";
 
 foreach ($sql as $query) {
